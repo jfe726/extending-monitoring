16684c736dac75ab69ad6b644a77b04f528b4f38
Dominik Sachsenhofer <dominik.sachsenhofer@gmail.com>
2018-08-01 23:15:04 +0200

    Update remote_python_demo.py

--- lab2/remote_python_demo.py
+++ lab2/remote_python_demo.py
@@ -14,7 +14,6 @@ from collections import defaultdict, namedtuple
 logger = logging.getLogger(__name__)
 
 
-#class RemoteDemoPlugin():
 class RemoteDemoPlugin(RemoteBasePlugin):
     def initialize(self, **kwargs):
         self.args = {}
@@ -24,11 +23,10 @@ class RemoteDemoPlugin(RemoteBasePlugin):
         self.args['port'] = kwargs['config']['port']
         self.args['debug'] = kwargs['config']['debug']
 
-        print("ID=" + self.args['id'])
-        print("URL=" + self.args['url'])
-        print("PORT=" + self.args['port'])
-        print("DEBUG=" + self.args['debug'])
-        print()
+        logger.info("ID=" + self.args['id'])
+        logger.info("URL=" + self.args['url'])
+        logger.info("PORT=" + self.args['port'])
+        logger.info("DEBUG=" + self.args['debug'])
 
         return
     
@@ -41,31 +39,21 @@ class RemoteDemoPlugin(RemoteBasePlugin):
             content = self.query_url(self.args['url'] + ":" + self.args['port'])
             content = json.loads(content)
 
-            print("CONTENT=" + str(content))
-            print()
+            logger.info("CONTENT=" + str(content))
 
             custom_counter = int(content["counter"])
             custom_random = int(content["random"])
 
-            if self.args['debug'] == "false":
-                group = self.topology_builder.create_group(self.args['url'], self.args['url'])
+            group = self.topology_builder.create_group(self.args['url'], self.args['url'])
 
-                element = group.create_element(self.args['url'], self.args['url'])
+            element = group.create_element(self.args['url'], self.args['url'])
 
-                element.add_endpoint(self.args['url'], int(self.args['port']))
+            element.add_endpoint(self.args['url'], int(self.args['port']))
 
-                element.report_property('Time', str(datetime.datetime.now()))
+            element.report_property('Time', str(datetime.datetime.now()))
 
-                element.absolute(key="custom_counter", value=custom_counter)
-                element.absolute(key="custom_random", value=custom_random)
-            else:
-                print("CREATE GROUP: " + self.args['url'])
-                print("CREATE ELEMENT: " + self.args['url'])
-                print("ADD ENDPOINT: " + self.args['url'] + ":" + self.args['port'])
-                print("REPORT PROPERTY: " + str(datetime.datetime.now()))
-                print("REPORT METRIC: " + str(custom_counter))
-                print("REPORT METRIC: " + str(custom_random))
-                print()
+            element.absolute(key="custom_counter", value=custom_counter)
+            element.absolute(key="custom_random", value=custom_random)
 
             end_time = time.time()
 
@@ -95,19 +83,4 @@ class RemoteDemoPlugin(RemoteBasePlugin):
                 continue
 
         return content
-
-#class Test:
-#    @staticmethod
-#    def test1():
-#        id = "host-1"
-#        url = "http://35.233.65.127"
-#        port = "8769"
-#        debug = "true"
-#
-#        plugin = RemoteDemoPlugin()
-#        plugin.initialize(config={"id": id, "url": url, "port": port, "debug": debug"})
-#        plugin.query()
-#
-#
-#if __name__ == "__main__":
-#    Test.test1()
+    
